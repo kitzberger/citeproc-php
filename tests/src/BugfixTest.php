6b7ca5357ea72e40e34d622051fdbd07bf21758d
Sebastian Böttger <seboettg@gmail.com>
2020-12-06 12:08:32 +0100

    zwischenstand

--- tests/src/BugfixTest.php
+++ tests/src/BugfixTest.php
@@ -134,7 +134,16 @@ class BugfixTest extends TestCase
 
     public function testBugfixGithub92()
     {
-
         $this->runTestSuite('bugfix-github-92');
+    }
+
+    public function testBugfixGithub94()
+    {
+        $this->runTestSuite('bugfix-github-94');
+    }
+
+    public function testBugfixGithub95()
+    {
+        $this->runTestSuite('bugfix-github-95');
     }
 }
